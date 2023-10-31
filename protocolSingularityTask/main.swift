e6c867b371e7eb89827ca0013470c229a2c0f99d
Mariya Aliyeva <mariyaalyevaseo@gmail.com>
2023-10-31 13:54:10 +0600

    m: add solutions

--- protocolSingularityTask/main.swift
+++ protocolSingularityTask/main.swift
@@ -7,5 +7,42 @@
 
 import Foundation
 
-print("Hello, World!")
+protocol AdditionProtocol {
+    func add()
+}
 
+protocol MultiplicationProtocol {
+    func multiplication()
+}
+
+extension MultiplicationProtocol {
+    func sortArray() -> [Int] {
+        let a = Calculate.getArray(self as! Calculate)
+        return a().sorted()
+    }
+}
+
+class Calculate: AdditionProtocol, MultiplicationProtocol {
+    
+    private var array: [Int]
+    init(arr: [Int]) {
+        self.array = arr
+    }
+    
+    func getArray() -> [Int] {
+        return self.array
+    }
+    
+    func add() {
+        print(array.reduce(0, +))
+    }
+    func multiplication() {
+        print(array.reduce(1, *))
+    }
+}
+
+
+var calculate = Calculate(arr: [10, 20, 30, 60, 5, 8])
+calculate.add()
+calculate.multiplication()
+print(calculate.sortArray())
